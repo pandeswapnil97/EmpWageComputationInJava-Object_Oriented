3c3a0b7d8c2bdd4b13c141cb1474d4300fcbf7d3
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-19 13:51:44 +0530

    Use Case 9 - Ability to save the Total

--- EmpWageComputation_Object_Oriented.java
+++ EmpWageComputation_Object_Oriented.java
@@ -1,10 +1,71 @@
 // Use Case 9 (Ability to save the Total Wage for each company)
 
+class CompanyEmpWage
+{
+	public static final int IS_FULL_TIME = 1;
+        public static final int IS_PART_TIME = 2;
+        
+
+        public static void empComputeWage(String companyName, int empRatePerHr, int numberOfWorkingDays, int maxWorkingHrs)
+        {
+                int empDailyHrs = 0;
+                int empTotalHrs = 0;
+
+                int EmpDailyWage;
+                int EmpTotalWage = 0;
+
+                int totalWorkingDays=0;
+
+                while(empTotalHrs < maxWorkingHrs && totalWorkingDays < numberOfWorkingDays)
+                {
+                        totalWorkingDays = totalWorkingDays + 1;
+
+                        int empCheck=(int)(Math.floor(Math.random()*10)%3);
+
+                        switch(empCheck)
+                        {
+                                case IS_FULL_TIME:
+                                {
+                                        empDailyHrs=8;
+                                        break;
+                                }
+                                case IS_PART_TIME:
+                                {
+                                        empDailyHrs=4;
+                                        break;
+                                }
+                        }
+
+                        empTotalHrs = empTotalHrs + empDailyHrs;
+
+                        EmpDailyWage = empRatePerHr * empDailyHrs;
+                        EmpTotalWage = EmpTotalWage + EmpDailyWage;
+
+                        System.out.println("Emlpoyee's Daily Hours for Day " + totalWorkingDays + " in " + companyName + ": " + empDailyHrs);
+                        System.out.println("Employee's Daily Wage for Day " + totalWorkingDays + " in " + companyName + ": " + EmpDailyWage);
+
+                        System.out.println();
+                }
+
+                System.out.println();
+                System.out.println();
+                System.out.println("Emlpoyee's Total Hours in " + companyName + ": "  + empTotalHrs);
+                System.out.println("Employee's Total Wage in " + companyName + ": "  + EmpTotalWage);
+		System.out.println();
+                System.out.println();
+		System.out.println();
+        }
+}
+
 
 public class EmpWageComputation_Object_Oriented
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation problem in Java (Object-Oriented)");
+		CompanyEmpWage dmart = new CompanyEmpWage();
+		dmart.empComputeWage("DMart", 20, 24, 100);
+
+		CompanyEmpWage bigbasket = new CompanyEmpWage();
+                bigbasket.empComputeWage("BigBasket", 25, 27, 98);
 	}
 }
