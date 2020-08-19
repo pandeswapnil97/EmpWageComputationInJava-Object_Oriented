e7682279f0de14483874a43c4b4f49d7c568f558
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-19 13:24:17 +0530

    Welcome to Employee Wage Computation problem in Java (Object-Oriented)

---
+++ EmpWageComputation_Object_Oriented.java
@@ -0,0 +1,10 @@
+// Use Case 9 (Ability to save the Total Wage for each company)
+
+
+public class EmpWageComputation_Object_Oriented
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Employee Wage Computation problem in Java (Object-Oriented)");
+	}
+}
